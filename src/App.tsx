09a800e416f3e81a3cedc943cdd23df1b7e4f170
gmichalopulos <gregomicha@gmail.com>
2021-10-30 00:19:41 -0300

    fix: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { ForceGraph2D } from 'react-force-graph';
-
 import './App.css';
 import BasicGraph from './components/basicGraph';
 import ColoredGraph from './components/coloredGraph';
@@ -9,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Roadmap Cósmico</h1>
-      {/* <BasicGraph length={40}/> */}
+      <BasicGraph length={40}/>
       <ColoredGraph />
     </div>
   );
